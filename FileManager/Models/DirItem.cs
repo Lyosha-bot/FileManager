ce698e0d54242281c530b3d96e5c19939587b182
Lyosha <owoshka@mail.ru>
2025-01-15 21:11:54 +0300

    Добавьте файлы проекта.

---
+++ FileManager/Models/DirItem.cs
@@ -0,0 +1,222 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+using System.Diagnostics;
+
+using IWshRuntimeLibrary;
+using File = System.IO.File;
+
+
+
+namespace FileManager
+{
+    public class DirItem
+    {
+        public string Name { get; }
+        public string DisplayName { get; }
+        public string Type { get; }
+
+        public string Dir { get; }
+
+        public Button ItemButton { get; }
+
+        public DirItem(string dir)
+        {
+            Name = Path.GetFileName(dir);
+            Type = GetType(dir);
+            DisplayName = (Type == "drive" ? $"Диск {dir[0].ToString()}" : Name);
+
+            Dir = dir;
+
+            ItemButton = new Button();
+            ItemButton.Tag = dir;
+            ItemButton.Style = UI.Styles.Button();
+            ItemButton.ToolTip = DisplayName;
+
+            Grid grid = new Grid();
+            grid.Style = UI.Styles.Wrapper();
+
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(8, GridUnitType.Star) });
+            grid.RowDefinitions.Add(new RowDefinition { Height = new GridLength(2, GridUnitType.Star) });
+
+            Image img = new Image();
+            img.Style = UI.Styles.Image();
+            img.Stretch = Stretch.Uniform;
+            img.Source = new BitmapImage(GetIcon(dir));
+            img.IsHitTestVisible = false;
+
+            Grid.SetRow(img, 0);
+            grid.Children.Add(img);
+
+            TextBlock textBlock = new TextBlock();
+            textBlock.Style = UI.Styles.Name();
+            textBlock.TextWrapping = TextWrapping.NoWrap;
+            textBlock.TextTrimming = TextTrimming.CharacterEllipsis;
+            textBlock.Text = DisplayName;
+            textBlock.IsHitTestVisible = false;
+
+            Grid.SetRow(textBlock, 1);
+            grid.Children.Add(textBlock);
+
+            ItemButton.Content = grid;
+        }
+
+        public bool Exists()
+        {
+            return Directory.Exists(Dir) || File.Exists(Dir);
+        }
+
+        public bool CanAccess()
+        {
+            try
+            {
+                if (Type == "folder" || Type == "drive")
+                    Directory.GetFiles(Dir);
+                else
+                {
+                    FileStream file = File.OpenRead(Dir);
+                    file.Close();
+                }
+                return true;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show("Нет доступа к данной директории.", "Ошибка доступа", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("Данной директории не существует.", "Ошибка доступа", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка доступа директории: {ex.ToString()}.", "Ошибка доступа", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+            return false;
+        }
+
+        public void Open()
+        {
+            if (Type == "folder" || Type == "shortcut")
+                return;
+
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = Dir,
+                    Verb = "open",
+                    UseShellExecute = true
+                };
+
+                Process.Start(startInfo);
+            }
+            catch (FileNotFoundException)
+            {
+                MessageBox.Show("Данного файла не существует.", "Ошибка доступа", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка открытия файла {Dir}: {ex.Message}");
+            }
+        }
+
+        // Static
+
+        static public bool CanAccess(string dir)
+        {
+            try
+            {
+                if (GetType(dir) == "folder" || GetType(dir) == "drive")
+                    Directory.GetFiles(dir);
+                else
+                {
+                    FileStream file = File.OpenRead(dir);
+                    file.Close();
+                }
+                return true;
+            }
+            catch (UnauthorizedAccessException)
+            {
+                MessageBox.Show($"Нет доступа к данной директории.", "Ошибка доступа", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                MessageBox.Show("Данной директории не существует.", "Ошибка доступа", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка доступа директории: {ex.ToString()}.", "Ошибка доступа", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            return false;
+        }
+
+        static public string GetType(string dir)
+        {
+            if (dir == "")
+                return "system";
+
+            string[] split = dir.Split('\\');
+            if (split[1] == "")
+                return "drive";
+
+            FileAttributes attr = File.GetAttributes(dir);
+
+            if (attr.HasFlag(FileAttributes.Directory))
+                return "folder";
+
+            switch (Path.GetExtension(dir).ToUpperInvariant())
+            {
+                case ".PNG":
+                case ".JPG":
+                case ".JPEG":
+                    return "image";
+
+                //case ".LNK":
+                //    return "shortcut";
+
+                default:
+                    return "unknown";
+            }
+        }
+
+        static public string? GetParent(string dir)
+        {
+            if (dir == "")
+                return null;
+
+            if (GetType(dir) == "drive")
+                return "";
+
+            DirectoryInfo? parent = Directory.GetParent(dir);
+            if (parent == null)
+                return null;
+
+            return parent.FullName;
+        }
+
+        static public Uri GetIcon(string dir)
+        {
+            string type = GetType(dir);
+
+            if (type == "image")
+                return new Uri(dir, UriKind.Absolute);
+
+            return new Uri(@$"Assets\{type}.png", UriKind.Relative);
+        }
+
+        static public string? GetDir(Button? item)
+        {
+            if (item == null)
+                return null;
+
+            var tag = item.Tag;
+            if (tag == null)
+                return null;
+
+            return tag.ToString();
+        }
+    }
+}
---
+++ FileManager/UI/UI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace FileManager.UI
+{
+    public static class Styles
+    {
+        public static Style Button()
+        {
+            Style style = new Style(typeof(Button));
+            style.Setters.Add(new Setter { Property = Control.MarginProperty, Value = new Thickness(5) });
+            style.Setters.Add(new Setter { Property = Control.BackgroundProperty, Value = Brushes.Transparent });
+            style.Setters.Add(new Setter { Property = Control.BorderBrushProperty, Value = Brushes.Transparent });
+            style.Setters.Add(new Setter { Property = Control.BorderThicknessProperty, Value = new Thickness(1) });
+            return style;
+        }
+
+        public static Style Wrapper()
+        {
+            Style style = new Style();
+            style.Setters.Add(new Setter { Property = Control.WidthProperty, Value = 100.0 });
+            style.Setters.Add(new Setter { Property = Control.HeightProperty, Value = 100.0 });
+            return style;
+        }
+
+        public static Style Image()
+        {
+            Style style = new Style();
+            style.Setters.Add(new Setter { Property = Control.MaxHeightProperty, Value = 80.0 });
+            return style;
+        }
+
+        public static Style Name()
+        {
+            Style style = new Style();
+            style.Setters.Add(new Setter { Property = Control.VerticalAlignmentProperty, Value = VerticalAlignment.Center });
+            style.Setters.Add(new Setter { Property = Control.HorizontalAlignmentProperty, Value = HorizontalAlignment.Center });
+            return style;
+        }
+    }
+}
