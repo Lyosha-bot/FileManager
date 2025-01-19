4a8d052d3095fe491a511243b4db34caea3de671
Lyosha <owoshka@mail.ru>
2025-01-19 23:05:33 +0300

    Removed COM library

--- FileManager/Models/DirItem.cs
+++ FileManager/Models/DirItem.cs
@@ -5,10 +5,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Media;
 using System.Diagnostics;
 
-using IWshRuntimeLibrary;
 using File = System.IO.File;
-
-
 
 namespace FileManager;
 
